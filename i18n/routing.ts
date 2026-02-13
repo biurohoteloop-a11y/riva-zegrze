2a77ae811289a1fa3a752d6001c56d42eab88dab
Karol Sucharski <biuro.hoteloop@gmail.com>
2026-02-13 10:59:26 +0100

    Add pathnames routing for EN/PL

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,50 @@ import { createNavigation } from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['pl', 'en'],
   defaultLocale: 'pl',
-  localePrefix: 'as-needed'
+  localePrefix: 'as-needed',
+  pathnames: {
+    '/': '/',
+    '/apartamenty': {
+      pl: '/apartamenty',
+      en: '/apartments'
+    },
+    '/o-nas': {
+      pl: '/o-nas',
+      en: '/about'
+    },
+    '/kontakt': {
+      pl: '/kontakt',
+      en: '/contact'
+    },
+    '/aktywnosci': {
+      pl: '/aktywnosci',
+      en: '/activities'
+    },
+    '/galeria': {
+      pl: '/galeria',
+      en: '/gallery'
+    },
+    '/rezerwacja': {
+      pl: '/rezerwacja',
+      en: '/reservation'
+    },
+    '/dane-firmy': {
+      pl: '/dane-firmy',
+      en: '/company-info'
+    },
+    '/polityka-prywatnosci': {
+      pl: '/polityka-prywatnosci',
+      en: '/privacy-policy'
+    },
+    '/regulamin': {
+      pl: '/regulamin',
+      en: '/terms-and-conditions'
+    },
+    '/informacje-o-rezerwacji': {
+      pl: '/informacje-o-rezerwacji',
+      en: '/booking-information'
+    }
+  }
 });
 
 export const { Link, redirect, usePathname, useRouter } = createNavigation(routing);
